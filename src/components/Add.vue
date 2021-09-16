d83e165f3e4cfd85eb170176d66032fb7a2f92e0
oussama smiai <smiai.oussama@gmail.com>
2021-09-16 08:41:15 +0100

    add and update restaurant

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,23 +1,17 @@
 <template>
 <Header/>
-<h1>Hello {{name}}, Welcome on Home Page</h1>
+<h1>Hello User, Welcome on Add restaurant Page</h1>
 </template>
 <script>
 import Header from './Header.vue'
 export default{
-    name: 'Home',
-    data(){
-        return{ 
-            name:''
-        }
-    },
+    name: 'Add',
     components:{
         Header
     },
     mounted()
     {
         let user = localStorage.getItem('user-info')
-        this.name = JSON.parse(user).name
         if (!user){
             this.$router.push({name:'SignUp'})
         }
--- src/components/Update.vue
+++ src/components/Update.vue
@@ -1,23 +1,17 @@
 <template>
 <Header/>
-<h1>Hello {{name}}, Welcome on Home Page</h1>
+<h1>Hello User, Welcome on Update Restaurant Page</h1>
 </template>
 <script>
 import Header from './Header.vue'
 export default{
-    name: 'Home',
-    data(){
-        return{ 
-            name:''
-        }
-    },
+    name: 'Update',
     components:{
         Header
     },
     mounted()
     {
         let user = localStorage.getItem('user-info')
-        this.name = JSON.parse(user).name
         if (!user){
             this.$router.push({name:'SignUp'})
         }
